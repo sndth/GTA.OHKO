02a8763bf793a6c005115c6212f278a14b12f8d3
sndth <75499293+sndth@users.noreply.github.com>
2025-12-26 21:37:53 +0100

    Refactor project structure and update build config

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,19 @@
-workspace("SA.OHKO")
+workspace("GTA.OHKO")
   -- Common settings
+  kind("SharedLib")
   location("build")
   cppdialect("C++latest")
   staticruntime("On")
+  targetextension(".asi")
   multiprocessorcompile("On")
 
   defines {
-    "_CRT_NON_CONFORMING_SWPRINTFS",
-    "GTASA",
-    "GTAGAME_NAME=\"San Andreas\"",
-    "GTAGAME_ABBR=\"SA\"",
-    "GTAGAME_ABBRLOW=\"sa\"",
-    "GTAGAME_PROTAGONISTNAME=\"CJ\"",
-    "GTAGAME_CITYNAME=\"San Andreas\"",
-    "PLUGIN_SGV_10US",
-    "RW"
+    "RW",
+    "_CRT_NON_CONFORMING_SWPRINTFS"
   } libdirs {
     "extern/plugin-sdk/output/lib"
   } includedirs {
     "extern",
-    "extern/plugin-sdk/plugin_sa",
-    "extern/plugin-sdk/plugin_sa/game_sa",
-    "extern/plugin-sdk/plugin_sa/game_sa/rw",
     "extern/plugin-sdk/shared",
     "extern/plugin-sdk/shared/game"
   } configurations {
@@ -29,21 +21,35 @@ workspace("SA.OHKO")
     "Release"
   }
 
+  -- Filter settings
+  filter("configurations:Debug")
+    symbols("Full")
+
+  filter("configurations:Release")
+    optimize("Full")
+
   -- Project settings
   project("SA.OHKO")
-    kind("SharedLib")
-    targetextension(".asi")
-
     files {
-      "source/**.cpp",
-      "source/**.hpp"
+      "source/game_sa/**.cpp",
+      "source/game_sa/**.json"
+    } defines {
+      "GTASA",
+      "GTAGAME_NAME=\"San Andreas\"",
+      "GTAGAME_ABBR=\"SA\"",
+      "GTAGAME_ABBRLOW=\"sa\"",
+      "GTAGAME_PROTAGONISTNAME=\"CJ\"",
+      "GTAGAME_CITYNAME=\"San Andreas\"",
+      "PLUGIN_SGV_10US"
+    } includedirs {
+      "extern/plugin-sdk/plugin_sa",
+      "extern/plugin-sdk/plugin_sa/game_sa",
+      "extern/plugin-sdk/plugin_sa/game_sa/rw"
     }
 
     -- Filter settings
-    filter "configurations:Debug"
-      links("plugin_d")
-      symbols("Full")
+    filter("configurations:Debug")
+      links("Plugin_d")
 
-    filter "configurations:Release"
-      links("plugin")
-      optimize("Full")
+    filter("configurations:Release")
+      links("Plugin")
